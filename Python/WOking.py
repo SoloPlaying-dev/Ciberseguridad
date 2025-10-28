b23b28636f4bc157a565e425f5114cafee9b62e6
SoloPlaying-dev <hipolitojra03@gmail.com>
2025-10-27 21:05:21 -0400

    lotery

--- Python/WOking.py
+++ Python/WOking.py
@@ -1,41 +1,23 @@
-def nombre(): #funcion to print name
-    print("Hipolito ES EL MEJOR")
-    nombre()
+import random
 
-def mostrar_mensaje(mensaje):
-    print("--------------------------------------------------------")
-    print(mensaje)
-    print("--------------------------------------------------------")
+def lot():
+    print("Welcome to the Lottery Game!")
+    print("Choose 3 unique numbers between 1 and 10.")
 
-def sumar():
-    valor1=int(input("Ingrese el primer numero a sumar"))
-    valor2=int(input("Ingrese el segundo numero a sumar: "))
-    suma=valor1+valor2
-    print("El Resultado de la Suma es: ",suma)
-
-    def restar():
-        valor1=int(input("Ingrese el primer numero a restar: "))
-        valor2=int(input("Ingrese el segundo numero a restar: "))
-        resta=valor1-valor2
-        print("El Resultado de la Resta es: ",resta)
-
-    def multiplicar():
-        valor1=int(input("Ingrese el primer numero a multiplicar: "))
-        valor2=int(input("Ingrese el segundo numero a multiplicar: "))
-        multiplicacion=valor1*valor2
-        print("El Resultado de la Multiplicacion es: ",multiplicacion)
-
-    def dividir():
-        valor1=int(input("Ingrese el primer numero a dividir: "))
-        valor2=int(input("Ingrese el segundo numero a dividir: "))
-        division=valor1/valor2
-        print("El Resultado de la Division es: ",division)
-
-    mostrar_mensaje("Fin de la Division")
-    mostrar_mensaje("Fin de la Suma")    
-    mostrar_mensaje("Fin de la Resta")
-    mostrar_mensaje("Fin de la Multiplicacion")
-    restar()
-    multiplicar()
-    dividir()
-    sumar()
+    user_numbers = set()
+    while len(user_numbers) < 3:
+        try:
+            number = int(input("Enter a number: "))
+            if 1 <= number <= 10:
+                user_numbers.add(number)
+            else:
+                print("Invalid number. Please choose a number between 1 and 10.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    print("Your chosen numbers are:", user_numbers)
+    lottery_numbers = set(random.sample(range(1, 11), 3))
+    print("The lottery numbers are:", lottery_numbers)
+    if user_numbers == lottery_numbers:
+        print("Congratulations! You've won the lottery!")
+    else:
+        print("Sorry, better luck next time!")
