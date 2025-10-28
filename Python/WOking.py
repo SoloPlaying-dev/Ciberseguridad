2cee99dbe7ca0ce0ef8c7d70787af484d876a61e
SoloPlaying-dev <hipolitojra03@gmail.com>
2025-10-27 21:24:23 -0400

    trying

--- Python/WOking.py
+++ Python/WOking.py
@@ -6,18 +6,26 @@ def lot():
 
     user_numbers = set()
     while len(user_numbers) < 3:
+
         try:
             number = int(input("Enter a number: "))
             if 1 <= number <= 10:
                 user_numbers.add(number)
+
             else:
                 print("Invalid number. Please choose a number between 1 and 10.")
+                print("Your chosen numbers are:", user_numbers)
         except ValueError:
+
             print("Invalid input. Please enter a valid number.")
-    print("Your chosen numbers are:", user_numbers)
-    lottery_numbers = set(random.sample(range(1, 11), 3))
-    print("The lottery numbers are:", lottery_numbers)
-    if user_numbers == lottery_numbers:
-        print("Congratulations! You've won the lottery!")
-    else:
-        print("Sorry, better luck next time!")
+            print("Your chosen numbers are:", user_numbers)
+
+            lottery_numbers = set(random.sample(range(1, 11), 3))
+            print("The lottery numbers are:", lottery_numbers)
+
+            if user_numbers == lottery_numbers:
+                print("Congratulations! You've won the lottery!")
+                print("Your winning numbers are:", lottery_numbers)
+            else:     
+                print("Sorry, better luck next time!")
+                print("Your chosen numbers are:", user_numbers)
